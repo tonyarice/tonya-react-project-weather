d3ddadcc166bda0bfe26b7161534831828ff274f
tonyarice <tonyacrice@gmail.com>
2021-08-17 11:58:06 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Tonya Rice and is {" "}
       <a href="https://github.com/tonyarice/tonya-react-project-weather" target="blank" rel="noreferrer">open-sourced on GitHub</a>.
       </footer>
+    </div>
     </div>
   );
 }
